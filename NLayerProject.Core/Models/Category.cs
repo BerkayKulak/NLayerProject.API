cfdcc94cafefa3ff04489207fd8e3e62ec8a04ef
Berkay Kulak <beko_468@hotmail.com>
2021-06-13 22:19:37 +0300

    Add project files.

---
+++ NLayerProject.Core/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Models
+{
+    public class Category
+    {
+        // ilk oluşturulduğu anda kategori bir tane boş bir collection nesnesi oluştursun.
+        public Category()
+        {
+            Product = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Product> Product { get; set; }
+
+    }
+}
---
+++ NLayerProject.Core/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+
+        //Entity framework bu kategori üzerinden inherit kullanarak tracking yapacak değişiklikleri izlicek. 
+        public virtual Category Category { get; set; }
+
+
+    }
+}
